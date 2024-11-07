772b322b962db3cd27e3ca36871121d2e8d7a59a
JuliaRiver <0000110768254xsp@al.educacao.sp.gov.br>
2024-11-06 22:23:44 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,51 +1,5 @@
 criaCartao(
-    'Conhecimentos Gerais: Medicina',
-    '1. Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?
-a) Tem entre 2 a 4 litros. São retirados 450 mililitros
-b) Tem entre 4 a 6 litros. São retirados 450 mililitros
-c) Tem 10 litros. São retirados 2 litros
-d) Tem 7 litros. São retirados 1,5 litros
-e) Tem 0,5 litros. São retirados 0,5 litros',
-    'Alternativa b: Entre 4 a 6 litros. São retirados 450 mililitros.'
-)
-
-criaCartao(
-    'Conhecimentos Gerais: Geografia',
-    '1 - Geologia é a ciência que estuda...
-a) os climas existentes no mundo
-b) os tipos de vegetações
-c) a elaboração de mapas
-d) a crosta terrestre
-e) se um país é populoso e/ou povoado',
-    'd) a crosta terrestre'
-)
-
-criaCartao(
-    'Conhecimentos Gerais: Filosofia',
-    '2. De quem é a famosa frase “Penso, logo existo”?
-a) Platão
-b) Galileu Galilei
-c) Descartes
-d) Sócrates
-e) Francis Bacon
-
-',
-    'Alternativa c: Descartes.
-
-“Je pense, donc je suis” é a frase original, escrita em francês, do filósofo René Descartes (1596-1650). Ela resume o pensamento e o método de Descartes, para quem tudo tem início na dúvida.'
-)
-
-criaCartao(
-    'Conhecimentos Gerais: História',
-    '5. Qual o nome do presidente do Brasil que ficou conhecido como Jango?
-a) Jânio Quadros
-b) Jacinto Anjos
-c) Getúlio Vargas
-d) João Figueiredo
-e) João Goulart
-
-',
-    'Alternativa e: João Goulart.
-
-João Belchior Marques Goulart (1919-1976) foi o 24º presidente do Brasil, cujo período de governo compreende os anos de 1961 a 1964.'
-)
+    'Teologia',
+    'Quem foi Jesus?',
+    'Messias de Deus'
+    )
