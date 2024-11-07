2f605a65b40ffb368521ea62859a6d0a3e3f4a0f
JuliaRiver <0000110768254xsp@al.educacao.sp.gov.br>
2024-11-06 22:11:53 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,55 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    // 'Conhecimentos Gerai:Medicina',
+    '1. Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?
+a) Tem entre 2 a 4 litros. São retirados 450 mililitros
+b) Tem entre 4 a 6 litros. São retirados 450 mililitros
+c) Tem 10 litros. São retirados 2 litros
+d) Tem 7 litros. São retirados 1,5 litros
+e) Tem 0,5 litros. São retirados 0,5 litros',
+    'Alternativa b: Entre 4 a 6 litros. São retirados 450 mililitros.
+
+A quantidade de sangue varia de pessoa para pessoa. O volume de sangue é entre 7% e 8% o peso corporal. Assim, um adulto com 50 e 80 quilos, pode ter entre 4 e 6 litros de sangue, respectivamente.
+
+Portanto, 4 a 6 litros é a alternativa que compreende a quantidade mais próxima de adultos com pesos diferentes.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Conhecimentos Gerais: Geografia',
+    '1 - Geologia é a ciência que estuda...
+a) os climas existentes no mundo
+b) os tipos de vegetações
+c) a elaboração de mapas
+d) a crosta terrestre
+e) se um país é populoso e/ou povoado',
+    'd) a crosta terrestre'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Conhecimentos Gerais: Filosofia',
+    '2. De quem é a famosa frase “Penso, logo existo”?
+a) Platão
+b) Galileu Galilei
+c) Descartes
+d) Sócrates
+e) Francis Bacon
+
+',
+    'Alternativa c: Descartes.
+
+“Je pense, donc je suis” é a frase original, escrita em francês, do filósofo René Descartes (1596-1650). Ela resume o pensamento e o método de Descartes, para quem tudo tem início na dúvida.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Conhecimentos Gerais: História',
+    '5. Qual o nome do presidente do Brasil que ficou conhecido como Jango?
+a) Jânio Quadros
+b) Jacinto Anjos
+c) Getúlio Vargas
+d) João Figueiredo
+e) João Goulart
+
+',
+    'Alternativa e: João Goulart.
+
+João Belchior Marques Goulart (1919-1976) foi o 24º presidente do Brasil, cujo período de governo compreende os anos de 1961 a 1964.'
 )
