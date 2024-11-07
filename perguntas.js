5d7267f6352030038ff869b0d803a2f810b4db85
JuliaRiver <0000110768254xsp@al.educacao.sp.gov.br>
2024-11-06 22:28:31 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,51 @@
 criaCartao(
     'Teologia',
     'Quem foi Jesus?',
+    'Jesus de Nazaré, também chamado Jesus Cristo, foi um pregador e líder religioso judeu '
+    )
+
+criaCartao(
+    'Teologia',
+    'Quem foi Jesus?',
+    'Messias de Deus'
+    )
+criaCartao(
+    'Teologia',
+    'Quem foi Jesus?',
+    'Messias de Deus'
+    )
+criaCartao(
+    'Teologia',
+    'Quem foi Jesus?',
+    'Messias de Deus'
+    )
+criaCartao(
+    'Teologia',
+    'Quem foi Jesus?',
+    'Messias de Deus'
+    )
+criaCartao(
+    'Teologia',
+    'Quem foi Jesus?',
+    'Messias de Deus'
+    )
+criaCartao(
+    'Teologia',
+    'Quem foi Jesus?',
+    'Messias de Deus'
+    )
+criaCartao(
+    'Teologia',
+    'Quem foi Jesus?',
+    'Messias de Deus'
+    )
+criaCartao(
+    'Teologia',
+    'Quem foi Jesus?',
+    'Messias de Deus'
+    )
+criaCartao(
+    'Teologia',
+    'Quem foi Jesus?',
     'Messias de Deus'
     )
